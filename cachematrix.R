20e0ba252d95f88922693342ce43092773b6f37d
dumasfr <dumasfr@gmail.com>
2015-05-20 19:21:37 -0400

    first version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the overall function calculates the inverse of a matrix, if the inverse of the matrix
+## already exist, the cached value is returned
 
-## Write a short comment describing this function
+## the following function takes a matrix as an input and return
+## a list containing 4 functions
 
 makeCacheMatrix <- function(x = matrix()) {
+  solved_matrix<-NULL
+  set<-function(y){                                   ##set function
+    x<<-y
+    solved_matrix<<-NULL
+  }
+  get<-function(){x}                                  ##get function
+  set_matrix<-function(s_mat){solved_matrix<<-s_mat}  ##set_matrix function
+  get_matrix<-function(){solved_matrix}               ##get_matrix function
+  list(set=set, get=get,
+       set_matrix=set_matrix,
+       get_matrix=get_matrix)                         ##the output of the function is a
+}                                                     ##list containing the functions
 
-}
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  solved_matrix<-x$get_matrix()     ##use the get_matrix function on the data
+  if(!is.null(solved_matrix)){      ##if the data exist (not NULL) then
+    message("getting cached data")  ##write "getting cached data" in terminal
+    return(solved_matrix)           ##return the solved matrix as the result of the function
+  }                                 ##if the data does not exist
+  data<-x$get()                     ##use the get() function to retrieve the non-solved matrix
+  solved_matrix<-solve(data,...)    ##solve it
+  x$set_matrix(solved_matrix)       ##cache the new result using the set_matrix() function
+  solved_matrix                     ##return the solved matrix as a result of the function
 }
